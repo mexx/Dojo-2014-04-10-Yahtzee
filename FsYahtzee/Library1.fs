a1ce9786690a1760408e7a9acbc7bc22990ea5b4
Max Malook <community@malook.de>
2014-04-10 20:19:07 +0200

    count all Twos

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -5,10 +5,18 @@ open FsUnit.Xunit
 
 type Dice = One | Two 
 
-type Category = Ones
+type Category = Ones | Twos
 
 let score (dOne, dTwo, dThree, dFour, dFive) category =
-    [dOne; dTwo; dThree; dFour; dFive] |> List.filter (fun x -> x = Dice.One) |> List.length
+    let diceToCount, diceScore =
+        match category with
+        | Ones -> One, 1
+        | Twos -> Two, 2
+
+    [dOne; dTwo; dThree; dFour; dFive]
+    |> List.filter ((=) diceToCount)
+    |> List.length
+    |> (*) diceScore
 
 [<Fact>]
 let ``Five dices with one placed in ones category`` ()= 
@@ -29,3 +37,10 @@ let ``One dice One placed in category Ones`` () =
     let throw =  Dice.One, Dice.Two, Dice.Two, Dice.Two, Dice.Two
     let category = Category.Ones
     score throw category |> should equal 1
+
+[<Fact>]
+let ``Five dices with two placed in twos category`` ()= 
+    let throw = Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
+    let category = Category.Twos
+    score throw category
+    |> should equal 10

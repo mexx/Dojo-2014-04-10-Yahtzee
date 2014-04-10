769d49215845b2b71569161f5e5a91842152d29e
Max Malook <community@malook.de>
2014-04-10 20:03:57 +0200

    Only Five Dices allowed

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -3,16 +3,24 @@
 open Xunit
 open FsUnit.Xunit
 
-type Dice = One
+type Dice = One | Two
 
 type Category = Ones
 
-let score throw category =
-    6
+let score (dOne, _, _, _, _) category =
+    if dOne = Dice.One then 6 else 0
 
 [<Fact>]
-let ``Six dices with one placed in ones category`` ()= 
-    let throw = [Dice.One; Dice.One; Dice.One; Dice.One; Dice.One; Dice.One]
+let ``Five dices with one placed in ones category`` ()= 
+    let throw = Dice.One, Dice.One, Dice.One, Dice.One, Dice.One
     let category = Category.Ones
     score throw category
     |> should equal 6
+
+[<Fact>]
+let ``Five dices with two placed in ones category`` () =
+    let throw =Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
+    let category = Category.Ones
+    score throw category
+    |> should equal 0
+

92d9f910b73f67b304f795008cc832115942effd
Max Malook <community@malook.de>
2014-04-10 20:38:41 +0200

    add pair category

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -1,22 +1,35 @@
 ﻿module FsYahtzee
 
 open Xunit
+open Xunit.Extensions
 open FsUnit.Xunit
 
-type Dice = One | Two 
+type Dice = One | Two | Three
 
-type Category = Ones | Twos
+type Category = Ones | Twos | Threes | Pair
 
-let score (dOne, dTwo, dThree, dFour, dFive) category =
-    let diceToCount, diceScore =
-        match category with
-        | Ones -> One, 1
-        | Twos -> Two, 2
+let score (dOne, dTwo, dThree, dFour, dFive) =
+    let diceScore = function | One -> 1 | Two -> 2 | Three -> 3
+    let throw = [dOne; dTwo; dThree; dFour; dFive]
+    function
+    | Pair ->
+        throw
+        |> List.sort
+        |> List.rev
+        |> List.head
+        |> diceScore
+        |> (*) 2
+    | category -> 
+        let diceToCount =
+            match category with
+            | Ones -> One
+            | Twos -> Two
+            | Threes -> Three
 
-    [dOne; dTwo; dThree; dFour; dFive]
-    |> List.filter ((=) diceToCount)
-    |> List.length
-    |> (*) diceScore
+        throw
+        |> List.filter ((=) diceToCount)
+        |> List.length
+        |> (*) (diceScore diceToCount)
 
 [<Fact>]
 let ``Five dices with one placed in ones category`` ()= 
@@ -44,3 +57,31 @@ let ``Five dices with two placed in twos category`` ()=
     let category = Category.Twos
     score throw category
     |> should equal 10
+
+[<Fact>]
+let ``Five dices with two placed in threes category`` ()= 
+    let throw = Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
+    let category = Category.Threes
+    score throw category
+    |> should equal 0
+
+[<Fact>]
+let ``Five dices with three placed in threes category`` ()= 
+    let throw = Dice.Three, Dice.Three, Dice.Three, Dice.Three, Dice.Three
+    let category = Category.Threes
+    score throw category
+    |> should equal 15
+
+[<Fact>]
+let ``Five dices with pair of twos and rest ones placed in pair category`` ()= 
+    let throw = Dice.One, Dice.One, Dice.One, Dice.Two, Dice.Two
+    let category = Category.Pair
+    score throw category
+    |> should equal 4
+
+[<Fact>]
+let ``Five dices with one placed in pair category`` ()= 
+    let throw = Dice.One, Dice.One, Dice.One, Dice.One, Dice.One
+    let category = Category.Pair
+    score throw category
+    |> should equal 2

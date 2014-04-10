5d678b018c1441fc31a4f21e53e1de9f522ed33d
Max Malook <community@malook.de>
2014-04-10 21:14:43 +0200

    add of a kind categories

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -6,17 +6,23 @@ open FsUnit.Xunit
 
 type Dice = One | Two | Three
 
-type Category = Ones | Twos | Threes | Pair
+type Category = Ones | Twos | Threes | Pair | ``Three of a kind``
 
 let diceScore = function | One -> 1 | Two -> 2 | Three -> 3
 
-let calculatePair throw =
+let rec allAreEqual array =
+    match array with
+    | f :: s :: rest when f <> s -> false
+    | f :: s :: rest when f = s -> (s :: rest) |> allAreEqual
+    | f :: [] -> true
+
+let calculateOfAKind count throw =
     throw
-    |> List.sort
-    |> List.rev
-    |> List.head
-    |> diceScore
-    |> (*) 2
+    |> List.sort |> List.rev |> Seq.windowed count
+    |> Seq.map ((<|) List.ofArray)
+    |> Seq.filter ((<|) allAreEqual)
+    |> Seq.head |> List.head
+    |> diceScore |> (*) count
 
 let calculateSingle throw category =
     let diceToCount =
@@ -29,65 +35,74 @@ let calculateSingle throw category =
     |> List.length
     |> (*) (diceScore diceToCount)
 
-
 let score (dOne, dTwo, dThree, dFour, dFive) =
     let throw = [dOne; dTwo; dThree; dFour; dFive]
     function
-    | Pair -> calculatePair throw
+    | Pair -> calculateOfAKind 2 throw
+    | ``Three of a kind`` -> calculateOfAKind 3 throw
     | category -> calculateSingle throw category
 
 [<Fact>]
-let ``Five dices with one placed in ones category`` ()= 
-    let throw = Dice.One, Dice.One, Dice.One, Dice.One, Dice.One
-    let category = Category.Ones
+let ``one placed in ones category`` ()= 
+    let throw = One, One, One, One, One
+    let category = Ones
     score throw category
     |> should equal 5
 
 [<Fact>]
-let ``Five dices with two placed in ones category`` () =
-    let throw =Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
-    let category = Category.Ones
+let ``two placed in ones category`` () =
+    let throw =Two, Two, Two, Two, Two
+    let category = Ones
     score throw category
     |> should equal 0
 
 [<Fact>]
 let ``One dice One placed in category Ones`` () =
-    let throw =  Dice.One, Dice.Two, Dice.Two, Dice.Two, Dice.Two
-    let category = Category.Ones
+    let throw =  One, Two, Two, Two, Two
+    let category = Ones
     score throw category |> should equal 1
 
 [<Fact>]
-let ``Five dices with two placed in twos category`` ()= 
-    let throw = Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
-    let category = Category.Twos
+let ``two placed in twos category`` ()= 
+    let throw = Two, Two, Two, Two, Two
+    let category = Twos
     score throw category
     |> should equal 10
 
 [<Fact>]
-let ``Five dices with two placed in threes category`` ()= 
-    let throw = Dice.Two, Dice.Two, Dice.Two, Dice.Two, Dice.Two
-    let category = Category.Threes
+let ``two placed in threes category`` ()= 
+    let throw = Two, Two, Two, Two, Two
+    let category = Threes
     score throw category
     |> should equal 0
 
 [<Fact>]
-let ``Five dices with three placed in threes category`` ()= 
-    let throw = Dice.Three, Dice.Three, Dice.Three, Dice.Three, Dice.Three
-    let category = Category.Threes
+let ``three placed in threes category`` ()= 
+    let throw = Three, Three, Three, Three, Three
+    let category = Threes
     score throw category
     |> should equal 15
 
 [<Fact>]
-let ``Five dices with pair of twos and rest ones placed in pair category`` ()= 
-    let throw = Dice.One, Dice.One, Dice.One, Dice.Two, Dice.Two
-    let category = Category.Pair
+let ``pair of twos and rest ones placed in pair category`` ()= 
+    let throw = One, One, One, Two, Two
+    let category = Pair
     score throw category
     |> should equal 4
 
 [<Fact>]
-let ``Five dices with one placed in pair category`` ()= 
-    let throw = Dice.One, Dice.One, Dice.One, Dice.One, Dice.One
-    let category = Category.Pair
+let ``one placed in pair category`` ()= 
+    let throw = One, One, One, One, One
+    let category = Pair
     score throw category
     |> should equal 2
 
+[<Fact>]
+let ``pair of ones pair of twos and one three placed in Pair category`` () =
+    let throw = Three, One, Two, One, Two
+    score throw Pair |> should equal 4
+
+[<Fact>]
+let ``three of ones pair of twos placed in Triple category`` () =
+    let throw = One, Two, One, Two, One
+    score throw ``Three of a kind`` |> should equal 3

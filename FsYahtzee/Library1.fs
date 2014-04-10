ea89f55b007354ed96e62e91859ca36730c1bbd7
Max Malook <community@malook.de>
2014-04-10 19:50:59 +0200

    all ones in ones category

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -3,7 +3,16 @@
 open Xunit
 open FsUnit.Xunit
 
+type Dice = One
+
+type Category = Ones
+
+let score throw category =
+    6
+
 [<Fact>]
 let ``Six dices with one placed in ones category`` ()= 
-    true
-    |> should equal true
+    let throw = [Dice.One; Dice.One; Dice.One; Dice.One; Dice.One; Dice.One]
+    let category = Category.Ones
+    score throw category
+    |> should equal 6

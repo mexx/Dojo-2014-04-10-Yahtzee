fa361a11221d7f4c7980a34617afce1ace0d6a82
Max Malook <community@malook.de>
2014-04-10 20:11:36 +0200

    count all Ones

--- FsYahtzee/Library1.fs
+++ FsYahtzee/Library1.fs
@@ -3,19 +3,19 @@
 open Xunit
 open FsUnit.Xunit
 
-type Dice = One | Two
+type Dice = One | Two 
 
 type Category = Ones
 
-let score (dOne, _, _, _, _) category =
-    if dOne = Dice.One then 6 else 0
+let score (dOne, dTwo, dThree, dFour, dFive) category =
+    [dOne; dTwo; dThree; dFour; dFive] |> List.filter (fun x -> x = Dice.One) |> List.length
 
 [<Fact>]
 let ``Five dices with one placed in ones category`` ()= 
     let throw = Dice.One, Dice.One, Dice.One, Dice.One, Dice.One
     let category = Category.Ones
     score throw category
-    |> should equal 6
+    |> should equal 5
 
 [<Fact>]
 let ``Five dices with two placed in ones category`` () =
@@ -24,3 +24,8 @@ let ``Five dices with two placed in ones category`` () =
     score throw category
     |> should equal 0
 
+[<Fact>]
+let ``One dice One placed in category Ones`` () =
+    let throw =  Dice.One, Dice.Two, Dice.Two, Dice.Two, Dice.Two
+    let category = Category.Ones
+    score throw category |> should equal 1
